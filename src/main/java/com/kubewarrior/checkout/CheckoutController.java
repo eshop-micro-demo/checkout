9f631e9f86d2e0c8a5957b1897e1c43a78493eb9
amitdeshpande <amitdeshpande@in.ibm.com>
2021-05-24 12:46:07 +0530

    Enabled CORS

--- src/main/java/com/kubewarrior/checkout/CheckoutController.java
+++ src/main/java/com/kubewarrior/checkout/CheckoutController.java
@@ -6,16 +6,14 @@ import com.kubewarrior.checkout.repository.UserorderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600, allowCredentials = "true")
 public class CheckoutController {
 
     @Autowired
